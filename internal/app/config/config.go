4bef5c9f50d9e790c2f82a9fa1ef5a5b640d748e
Yancharuk Alexander <alex@itvault.info>
2025-06-12 07:53:44 +0300

    chore: Update linter config
    closes: #96

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -21,7 +21,7 @@ type Conf struct {
 	Description      string        `conf:"default:Go microservice,short:d,help:Project short description"`
 	ModuleName       string        `conf:"default:github.com/test/test,short:m,help:Go module name"`
 	GoImage          string        `conf:"default:nafigat0r/go:1.24.3,short:g,help:Go docker image"`
-	LinterImage      string        `conf:"default:nafigat0r/golangci-lint:2.0.2,short:l,help:Linter docker image"`
+	LinterImage      string        `conf:"default:nafigat0r/golangci-lint:2.1.6,short:l,help:Linter docker image"`
 	GovulncheckImage string        `conf:"default:nafigat0r/govulncheck:1.1.4,short:c,help:Govulncheck docker image"`
 	ShutdownTimeout  time.Duration `conf:"default:10s,short:t,help:Timeout for graceful shutdown"`
 	WithREST         bool          `conf:"default:false,short:r,help:Add HTTP server with REST API functionality"`
