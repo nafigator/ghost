5a758c535edb0e33967e84842a73f83d8d8c4e69
Yancharuk Alexander <alex@itvault.info>
2026-09-05 06:29:56 +0300

    refactor: Implement discrete templating

--- internal/app/init.go
+++ internal/app/init.go
@@ -10,100 +10,114 @@ type tp struct {
 	dir  string
 	file string
 	src  string
-	inc  []string
 }
 
 type tps map[string]tp
 
 var (
-	//go:embed templates/cmd/main.gotmpl
+	//go:embed templates/default/cmd/main.gotmpl
 	mainSrc string
 
-	//go:embed templates/gomod.gotmpl
+	//go:embed templates/default/gomod.gotmpl
 	gomodSrc string
 
-	//go:embed templates/gosum.gotmpl
+	//go:embed templates/default/gosum.gotmpl
 	gosumSrc string
 
-	//go:embed templates/golangci.gotmpl
+	//go:embed templates/default/golangci.gotmpl
 	golangciSrc string
 
-	//go:embed templates/gitignore.gotmpl
+	//go:embed templates/default/gitignore.gotmpl
 	gitignoreSrc string
 
-	//go:embed templates/makefile.gotmpl
+	//go:embed templates/default/makefile.gotmpl
 	makefileSrc string
 
-	//go:embed templates/includes/rest.gotmpl
-	withREST string
+	//go:embed templates/rest/default/makefile.gotmpl
+	restMakefileSrc string
 
-	//go:embed templates/compose.gotmpl
+	//go:embed templates/default/compose.gotmpl
 	composeSrc string
 
-	//go:embed templates/build/compose.override.gotmpl
+	//go:embed templates/default/build/compose.override.gotmpl
 	composeOverrideSrc string
 
-	//go:embed templates/build/zapper.gotmpl
+	//go:embed templates/rest/default/build/compose.override.gotmpl
+	restComposeOverrideSrc string
+
+	//go:embed templates/default/build/zapper.gotmpl
 	zapperSrc string
 
-	//go:embed templates/internal/app/app.gotmpl
+	//go:embed templates/default/internal/app/app.gotmpl
 	appSrc string
 
-	//go:embed templates/internal/app/shutdown.gotmpl
+	//go:embed templates/default/internal/app/shutdown.gotmpl
 	shutdownSrc string
 
-	//go:embed templates/internal/app/config/config.gotmpl
+	//go:embed templates/default/internal/app/config/config.gotmpl
 	configSrc string
 
-	//go:embed templates/internal/app/container/container.gotmpl
+	//go:embed templates/rest/default/internal/app/config/config.gotmpl
+	restConfigSrc string
+
+	//go:embed templates/default/internal/app/container/container.gotmpl
 	containerSrc string
 
-	//go:embed templates/internal/app/http/init.gotmpl
+	//go:embed templates/default/internal/app/http/init.gotmpl
 	httpInitSrc string
 
-	//go:embed templates/internal/app/http/mux.gotmpl
+	//go:embed templates/rest/default/internal/app/http/init.gotmpl
+	restHTTPInitSrc string
+
+	//go:embed templates/default/internal/app/http/mux.gotmpl
 	httpMuxSrc string
 
-	//go:embed templates/internal/app/http/handlers/support/build.gotmpl
+	//go:embed templates/rest/default/internal/app/http/mux.gotmpl
+	restHTTPMuxSrc string
+
+	//go:embed templates/default/internal/app/http/handlers/support/build.gotmpl
 	buildSrc string
 
-	//go:embed templates/internal/app/http/handlers/support/health.gotmpl
+	//go:embed templates/default/internal/app/http/handlers/support/health.gotmpl
 	healthSrc string
 
-	//go:embed templates/internal/app/http/handlers/support/log.gotmpl
+	//go:embed templates/default/internal/app/http/handlers/support/log.gotmpl
 	logSrc string
 
-	//go:embed templates/internal/app/http/handlers/support/response/response.gotmpl
+	//go:embed templates/default/internal/app/http/handlers/support/response/response.gotmpl
 	responseSrc string
 
-	//go:embed templates/internal/app/http/handlers/support/version.gotmpl
+	//go:embed templates/default/internal/app/http/handlers/support/version.gotmpl
 	versionSrc string
 
-	//go:embed templates/internal/app/http/handlers/api/index.gotmpl
+	//go:embed templates/rest/default/internal/app/http/handlers/api/index.gotmpl
 	indexAPISrc string
 
-	//go:embed templates/internal/app/http/validators/validators.gotmpl
+	//go:embed templates/default/internal/app/http/validators/validators.gotmpl
 	validatorsSrc string
 
-	//go:embed templates/internal/app/readiness/readiness.gotmpl
+	//go:embed templates/default/internal/app/readiness/readiness.gotmpl
 	readinessSrc string
 
-	//go:embed templates/internal/app/http/errors/errors.gotmpl
+	//go:embed templates/rest/default/internal/app/readiness/readiness.gotmpl
+	restReadinessSrc string
+
+	//go:embed templates/default/internal/app/http/errors/errors.gotmpl
 	errorsSrc string
 
-	//go:embed templates/internal/sdk/readiness/socket/socket.gotmpl
+	//go:embed templates/default/internal/sdk/readiness/socket/socket.gotmpl
 	readinessSocketSrc string
 
-	//go:embed templates/internal/sdk/http/mux/middleware.gotmpl
+	//go:embed templates/default/internal/sdk/http/mux/middleware.gotmpl
 	middlewareSrc string
 
-	//go:embed templates/internal/sdk/http/mux/mux.gotmpl
+	//go:embed templates/default/internal/sdk/http/mux/mux.gotmpl
 	sdkMuxSrc string
 
-	//go:embed templates/internal/sdk/http/mux/respond.gotmpl
+	//go:embed templates/default/internal/sdk/http/mux/respond.gotmpl
 	respondSrc string
 
-	//go:embed templates/internal/sdk/http/mux/validator.gotmpl
+	//go:embed templates/default/internal/sdk/http/mux/validator.gotmpl
 	validatorSrc string
 )
 
@@ -115,6 +129,46 @@ func templates(c *config.Conf) tps {
 			dir:  "internal/app/http/handlers/api",
 			file: "internal/app/http/handlers/api/index.go",
 			src:  indexAPISrc,
+		}
+
+		t["config"] = tp{
+			dir:  "internal/app/config",
+			file: "internal/app/config/config.go",
+			src:  restConfigSrc,
+		}
+
+		t["makefile"] = tp{
+			file: "Makefile",
+			src:  restMakefileSrc,
+		}
+
+		t["compose-override"] = tp{
+			file: "docker-compose.override.yml",
+			src:  restComposeOverrideSrc,
+		}
+
+		t["init"] = tp{
+			dir:  "internal/app/http", //nolint:goconst	// Don't change for readability.
+			file: "internal/app/http/init.go",
+			src:  restHTTPInitSrc,
+		}
+
+		t["mux"] = tp{
+			dir:  "internal/app/http",
+			file: "internal/app/http/mux.go",
+			src:  restHTTPMuxSrc,
+		}
+
+		t["readiness"] = tp{
+			dir:  "internal/app/readiness",
+			file: "internal/app/readiness/readiness.go",
+			src:  restReadinessSrc,
+		}
+
+		t["compose-override-dist"] = tp{
+			dir:  "build", //nolint:goconst	// Don't change for readability.
+			file: "build/docker-compose.override.dist.yml",
+			src:  restComposeOverrideSrc,
 		}
 	}
 
@@ -142,7 +196,6 @@ func common() tps { //nolint:funlen  // This function supposed to be longer than
 		"makefile": {
 			file: "Makefile",
 			src:  makefileSrc,
-			inc:  []string{withREST},
 		},
 		"compose": {
 			file: "docker-compose.yml",
@@ -151,13 +204,11 @@ func common() tps { //nolint:funlen  // This function supposed to be longer than
 		"compose-override": {
 			file: "docker-compose.override.yml",
 			src:  composeOverrideSrc,
-			inc:  []string{withREST},
 		},
 		"compose-override-dist": {
-			dir:  "build", //nolint:goconst	// Don't change for readability.
+			dir:  "build",
 			file: "build/docker-compose.override.dist.yml",
 			src:  composeOverrideSrc,
-			inc:  []string{withREST},
 		},
 		"zapper": {
 			dir:  "bin",
@@ -188,7 +239,6 @@ func common() tps { //nolint:funlen  // This function supposed to be longer than
 			dir:  "internal/app/config",
 			file: "internal/app/config/config.go",
 			src:  configSrc,
-			inc:  []string{withREST},
 		},
 		"container": {
 			dir:  "internal/app/container",
@@ -199,13 +249,11 @@ func common() tps { //nolint:funlen  // This function supposed to be longer than
 			dir:  "internal/app/http",
 			file: "internal/app/http/init.go",
 			src:  httpInitSrc,
-			inc:  []string{withREST},
 		},
 		"mux": {
 			dir:  "internal/app/http",
 			file: "internal/app/http/mux.go",
 			src:  httpMuxSrc,
-			inc:  []string{withREST},
 		},
 		"build": {
 			dir:  "internal/app/http/handlers/support", //nolint:goconst	// Don't change for readability.
